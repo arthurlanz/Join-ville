2dad37fcca248b344514221cdb3d7c12173460ec
gaSombrio <sombrio.ga@gmail.com>
2025-07-15 15:46:35 -0300

    FEAT: header sem o css e as funcionalidades #1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-
+  <header>
+    <p>Explore, Avalie e Viva Joinville de Verdade</p>
+    <div>
+      <p><a href="#"><img src="/public/logotipo.png" alt=""></a></p>
+      <nav>
+        <ul>
+          <li><a href="#">Gastronomia</a></li>
+          <li><a href="#">Clássicos de Joinville</a></li>
+          <li><a href="#">Festas e shows</a></li>
+          <li><a href="#">Esportes</a></li>
+          <li><a href="#">Atividades ao ar livre</a></li>
+          <li><a href="#">Cultura</a></li>
+        </ul>
+      </nav>
+        <button>
+          <img src="/public/Star 1.png" alt="">
+        </button>
+        <button>
+          <img src="/public/Group 37.png" alt="">
+        </button> 
+    </div>
+  </header>
 </template>
 
 <style scoped>
